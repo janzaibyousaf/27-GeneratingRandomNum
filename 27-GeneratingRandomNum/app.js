0bfcf2cdeec7fef4b36d9c2c608e54fd6abc516a
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-06-09 14:19:13 +0500

    Add files via upload

---
+++ 27-GeneratingRandomNum/app.js
@@ -0,0 +1,12 @@
+var random = Math.random();
+
+console.log(random);
+
+var random = Math.random() * 10
+
+console.log(random);
+
+
+var random = Math.round( Math.random() * 10)
+
+console.log(random);
